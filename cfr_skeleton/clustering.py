1179a735fc1058be7dc75bf5e6d081043c0b15ad
dyneleee <70602157+dyneleee@users.noreply.github.com>
2024-01-21 23:53:34 -0500

    andrew's k-means clustering added to cluster.py

--- cfr_skeleton/clustering.py
+++ cfr_skeleton/clustering.py
@@ -1,3 +1,8 @@
+import numpy as np
+import random as rd
+from sklearn.cluster import kmeans_plusplus
+from sklearn.cluster import KMeans
+
 import eval7
 import pickle
 
@@ -233,22 +238,58 @@ def preflop_hists(rd2_groups): # preflop to flop
 
     return rd1_hists
 
-
+def kmeanspp(data, k, iterations):
+    '''Run kmeans'''
+    darr = np.array(data)
+    kmeans = KMeans(n_clusters=k, init= 'k-means++', n_init = 5, max_iter=iterations)
+    kmeans.fit(darr)
+    cl = kmeans.labels_ # Contains labels of clusters
+    return cl 
+    
+    
+def samplingkmeanspp(samsize, data, k, iterations):
+    #data is a dict with that we're inputting 
+    '''sampling k-mean algorithm that outputs k clusters'''
+    # Convert to a group
+    a = data.items()
+    # Convert object to a list 
+    b = list(a)
+    #Take out hands from tuple (leave just histogram)
+    histlist = []
+    for i in range(len(b)):
+        histlist.append(b[i][1])
+    #Create random sample to do kmeans on (use rand if we're doing samplingkmean later)
+    #rand = rd.sample(histlist, samsize)
+    #Do kmeans
+    clusterlst = kmeanspp(histlist, k, iterations)
+    clmap = {}
+    for i in range(len(b)):
+        clmap[b[i[0]], clusterlst[i]]
+    return clmap
 
 finish_groups = {} # given, maps finishing state to groups 0-4
 
 rd4_hists = turn_hists(finish_groups) # maps 4th round state to histogram
-# rd4_groups = cluster(rd4_hists)
-rd4_groups = {} # maps 4th round state to groups
 
-rd3_hists = auction_hists(finish_groups)
+rd4_groups = samplingkmeanspp(10, rd4_hists, 5, 5) # maps 4th round state to groups
+
+rd3_hists = auction_hists(rd4_groups)
 # rd3_groups = cluster(rd3_hists)
-rd3_groups = {}
+rd3_groups = samplingkmeanspp(10, rd3_hists, 5, 5)
 
 rd2_hists = flop_hists(rd3_groups)
 # rd2_groups = cluster(rd2_hists)
-rd2_groups = {}
+rd2_groups = samplingkmeanspp(10, rd2_hists, 5, 5)
 
 rd1_hists = preflop_hists(rd2_groups)
 # rd1_groups = cluster(rd1_hists)
-rd1_groups = {} # should end up with 20 preflop clusters
+rd1_groups = samplingkmeanspp(10, rd1_hists, 5, 5) # should end up with 20 preflop clusters
+
+
+
+#def kmeanspp(data, k):
+#    '''Select starting nodes for kmeans'''
+#    d = np.array(data)
+#    centers, indices = kmeans_plusplus(d, n_clusters=k, random_state=None)
+#    return centers, indices
+
